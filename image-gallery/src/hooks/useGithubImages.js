91660c33555dc12661a4d9168143b7f21155eff4
Tonyy456 <ajdalesandro0115@gmail.com>
2024-03-21 16:13:04 -0400

    Documentation

---
+++ image-gallery/src/hooks/useGithubImages.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import { Octokit } from "octokit";
+import imageLocations from '../image-locations.json'
+
+/*
+author: Anthony D'Alesandro
+
+hook with the sole purposes of extracting data out of use given image-locations.json file and grabbing that data from github.
+*/
+function useGithubImages() {
+    const [images, setImages] = useState([]);
+    const octokit = new Octokit({
+      auth: process.env.REACT_APP_GITHUB_ACCESS_TOKEN
+     });
+    useEffect(() => {
+        async function onLoad() {
+            for(let i = 0; i < imageLocations.locations.length; i++) {
+                const location = imageLocations.locations[i];
+                const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+                    owner: location.owner,
+                    repo: location.repo,
+                    path: location.folderPath,
+                    headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                    }
+                })
+                const imageArr = response.data;
+                setImages(prev => prev.concat(imageArr));
+            }
+        }
+        onLoad();
+    },[])
+    return images;
+}
+
+export default useGithubImages
